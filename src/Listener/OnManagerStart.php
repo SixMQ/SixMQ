fe40124c0b9d10a4fed3a8f9c6600c83a8f340b8
宇润 <admin@yurunsoft.com>
2018-08-16 16:50:01 +0800

    删除无用代码

--- src/Listener/OnManagerStart.php
+++ src/Listener/OnManagerStart.php
@@ -29,39 +29,5 @@ class OnManagerStart implements IManagerStartEventListener
 		$process = ProcessManager::create('SixMQQueueMonitor');
 		$process->start();
 		echo 'Process [SixMQQueueMonitor] start', PHP_EOL;
-
-		// $taskHandler = new class implements ITaskHandler{
-		// 	/**
-		// 	 * 任务处理方法
-		// 	 * @param TaskParam $param
-		// 	 * @param \Swoole\Server $server
-		// 	 * @param integer $taskID
-		// 	 * @param integer $WorkerID
-		// 	 * @return void
-		// 	 */
-		// 	public function handle(TaskParam $param, \Swoole\Server $server, int $taskID, int $WorkerID)
-		// 	{
-		// 		var_dump('task');
-		// 	}
-
-		// 	/**
-		// 	 * 任务结束时触发
-		// 	 * @param \swoole_server $server
-		// 	 * @param int $taskId
-		// 	 * @param mixed $data
-		// 	 * @return void
-		// 	 */
-		// 	public function finish(\Swoole\Server $server, int $taskID, $data)
-		// 	{
-		// 		var_dump('finish');
-		// 	}
-		// };
-		// $taskParam = new TaskParam();
-		// $taskInfo = new TaskInfo($taskHandler, $taskParam);
-		// var_dump(TaskManager::post($taskInfo));
-
-		// var_dump('start', $e->server->getSwooleServer());
-
-		// var_dump('ms');
 	}
 }
