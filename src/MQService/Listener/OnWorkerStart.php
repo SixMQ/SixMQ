a7f56d02d110932c45fcf563cc2ea89e26b5cf95
Yurun <admin@yurunsoft.com>
2019-08-28 10:33:09 +0800

    修复问题

--- src/MQService/Listener/OnWorkerStart.php
+++ src/MQService/Listener/OnWorkerStart.php
@@ -4,6 +4,7 @@ namespace SixMQ\MQService\Listener;
 use Imi\Config;
 use Imi\Util\Imi;
 use Imi\Util\File;
+use Imi\Event\Event;
 use Imi\Event\EventParam;
 use Imi\Event\IEventListener;
 use Imi\Bean\Annotation\Listener;
@@ -22,7 +23,10 @@ class OnWorkerStart implements IEventListener
     {
         $this->update();
         // 暂定 1 分钟更新一次配置
-        swoole_timer_tick(60 * 1000, imiCallable([$this, 'update']));
+        $timerId = \Swoole\Timer::tick(60 * 1000, imiCallable([$this, 'update']));
+        Event::on('IMI.MAIN_SERVER.WORKER.EXIT', function() use($timerId){
+            \Swoole\Timer::clear($timerId);
+        });
     }
 
     public function update()
